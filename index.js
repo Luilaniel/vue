f5c99f47f69614bbda16f3bc14fba8bf9bf5930b
Luilaniel <karinakasko11@gmail.com>
2025-04-14 13:15:22 +0300

    Перший коміт

---
+++ index.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log("Сума:", sum(5, 3));
+console.log("Різниця:", subtract(5, 3));
+console.log("Добуток:", multiply(5, 3));
