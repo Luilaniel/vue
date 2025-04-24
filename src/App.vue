94cb7a34c7dea61984ec5f42397e98f2529f6d46
Luilaniel <karinakasko11@gmail.com>
2025-04-24 19:03:47 +0300

    Підключення bootstrap-vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ButtonComponent />
+  </div>
+</template>
+
+<script>
+import ButtonComponent from './components/ButtonComponent.vue'
+export default {
+  name: 'App',
+  components: {
+    ButtonComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
