fa9dc0c221642e0a6a49497349beabfd0a087c5a
Luilaniel <karinakasko11@gmail.com>
2025-05-13 20:05:28 +0300

    Додано компонент UserCard

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <SomeDirectives />
+    <UserCard v-bind="{ name: 'Karyna', age: 18, isActive: true }"/>
+    <UserCard v-bind="{ name: 'Marina', age: 23, isActive: false }"/>
+    <UserCard v-bind="{ name: 'Anna', age: 26, isActive: true }"/>
+    <UserCard />
   </div>
 </template>
 
 <script>
-import SomeDirectives from "@/components/SomeDirectives.vue";
+import UserCard from "@/components/UserCard.vue";
 
 export default {
   name: 'App',
   components: {
-    SomeDirectives
+    UserCard
   }
 }
 </script>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-card v-if="hasData" :title="`${ name }, ${ age }`">
+  <b-card-text v-if="isActive">Aktywny użytkownik</b-card-text>
+    <b-card-text v-else>Nieaktywny użytkownik</b-card-text>
+  </b-card>
+  <b-card v-else title="Brak danych">
+    <b-card-text>Brak danych o użytkowniku</b-card-text>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "UserCard",
+  props: {
+    name: String,
+    age: Number,
+    isActive: Boolean
+  },
+  computed: {
+    hasData() {
+      return this.name !== undefined && this.age !== undefined && this.isActive !== undefined;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
